2552e31b3d05f49b45828d745aece8af1b0fe06c
chrsmt <csmit@outlook.com>
2017-02-09 09:47:55 -0600

    add time to title

--- formatDate.js
+++ formatDate.js
@@ -3,9 +3,9 @@ function formatDate (timestamp, options) {
   var month = timestamp.getMonth()
   var date = timestamp.getDate()
   var day = timestamp.getDay()
-  var hour = timestamp.getHours()
-  var minute = timestamp.getMinutes()
-  var seconds = timestamp.getSeconds()
+  var hours = timestamp.getHours()
+  var minutes = ('0' + timestamp.getMinutes()).slice(-2)
+  var seconds = ('0' + timestamp.getSeconds()).slice(-2)
   var milliseconds = timestamp.getMilliseconds()
 
   // set day name
@@ -18,14 +18,19 @@ function formatDate (timestamp, options) {
     'July', 'August', 'September', 'October', 'November', 'December' ]
   var monthName = months[month]
 
+  // set time for 12-hour format
+  var period = hours >= 12 ? 'pm' : 'am'
+  var hours12 = hours % 12 || 12
+  var time = hours12 + ':' + minutes + period
+
   options = options || {}
 
   if (options.format === 'filename') {
     var filenameDate = year + ('0' + (month + 1)).slice(-2) + ('0' + date).slice(-2) +
-                       '_' + hour + minute + seconds + milliseconds
+                       '_' + hours + minutes + seconds + milliseconds
     return filenameDate
   } else if (options.format === 'title') {
-    var titleDate = dayName + ', ' + monthName + ' ' + date + ', ' + year
+    var titleDate = dayName + ', ' + monthName + ' ' + date + ', ' + year + ' - ' + time
     return titleDate
   } else {
     throw new Error('Date format not set')
