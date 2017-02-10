3f5b0571ddf468dc99f21e8d328e6d0f48612e4a
chrsmt <csmit@outlook.com>
2017-02-09 21:14:02 -0600

    update to js standard style

--- test/test3Spec.js
+++ test/test3Spec.js
@@ -1,29 +1,27 @@
-xdescribe('SPEC 3', function() {
+xdescribe('SPEC 3', function () {
+  it('3 should add a todo', function () {
+    browser.get('http://www.angularjs.org')
 
-    it('3 should add a todo', function() {
-        browser.get('http://www.angularjs.org');
+    element(by.model('todoList.todoText')).sendKeys('write a protractor test')
+    element(by.css('[value="add"]')).click()
 
-        element(by.model('todoList.todoText')).sendKeys('write a protractor test');
-        element(by.css('[value="add"]')).click();
+    var todoList = element.all(by.repeater('todo in todoList.todos'))
+    expect(todoList.count()).toEqual(3)
+    expect(todoList.count()).toEqual(6) // Failure
+    expect(todoList.get(2).getText()).toEqual('write a protractor test')
+  })
 
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.count()).toEqual(6); //Failure
-        expect(todoList.get(2).getText()).toEqual('write a protractor test');
-    });
+  it('3 second it', function () {
+    browser.get('http://www.angularjs.org')
 
-    it('3 second it', function() {
-        browser.get('http://www.angularjs.org');
+    element(by.model('todoList.todoText')).sendKeys('write a protractor test')
+    element(by.css('[value="add"]')).click()
 
-        element(by.model('todoList.todoText')).sendKeys('write a protractor test');
-        element(by.css('[value="add"]')).click();
-
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.count()).toEqual(3);
-        expect(todoList.get(2).getText()).toEqual('write a protractor test');
-    });
-
-});
+    var todoList = element.all(by.repeater('todo in todoList.todos'))
+    expect(todoList.count()).toEqual(3)
+    expect(todoList.count()).toEqual(3)
+    expect(todoList.count()).toEqual(3)
+    expect(todoList.count()).toEqual(3)
+    expect(todoList.get(2).getText()).toEqual('write a protractor test')
+  })
+})
